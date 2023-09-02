5abaa021e2a3d0977ec945e0e519d97611382abf
Daniel <danielfelipe2905@gmail.com>
2023-09-02 10:55:54 -0300

    Atualizados

--- project/app.js
+++ project/app.js
@@ -2,7 +2,11 @@ let express = require('express')
 const app = express()//cria uma copia inteira do framework express.js, logo ela representa o express.js agora
 
 app.get('/'/*rota inicial*/, function(req, res){
-  res.send('Seja bem vindo para minha aplicação!')
+  res.send('Olá mundo')
+})
+
+app.get('/voto'/*rota voto*/, function(req, res){
+  res.sendFile(__dirname + '/html/index.html')
 })
 
 app.get('/sobre'/*rota sobre*/, function(req, res){
@@ -10,7 +14,7 @@ app.get('/sobre'/*rota sobre*/, function(req, res){
 })
 
 app.get('/ola/:nome/:profissao'/*rota nome*/, function(req, res){
-  res.send(`<h1>Seja bem vindo ${req.params.nome}!</h1><br><h2>Sua profissão é: ${req.params.profissao}</h2>`)
+  res.sendFile(`<h1>Seja bem vindo ${req.params.nome}!</h1><br><h2>Sua profissão é: ${req.params.profissao}</h2>`)
 })
 
 app.listen(8081, function(){
