0c05321b5dcea4dc1ca9e853859e9a9e66229ae2
Daniel <danielfelipe2905@gmail.com>
2023-09-06 17:24:06 -0300

    add

--- project_2/index.js
+++ project_2/index.js
@@ -7,8 +7,17 @@ const Post = require('./models/Post')
 
 //config
 //template engine
-app.engine('handlebars', engine({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine(
+  'handlebars',
+  engine({
+    defaultLayout: 'main',
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+      allowProtoMethodsByDefault: true,
+    },
+  }),
+),
+  app.set('view engine', 'handlebars')
 
 //body Parser
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -22,7 +31,9 @@ const sequelize = new Sequelize('teste', 'root', '29052007', {
 
 //rotas
 app.get('/', function (req, res) {
-  res.render('formulario')
+  Post.findAll().then(function (posts) {
+    res.render('home', { posts: posts })
+  })
 })
 
 app.get('/cad', function (req, res) {
@@ -36,12 +47,20 @@ app.post('/add', function (req, res) {
     conteudo: req.body.conteudo,
   })
     .then(function () {
-      res.send('Post criado com sucesso')
+      res.redirect('/')
     })
     .catch(function (erro) {
       res.send(`Erro na criação, pelo seguinte erro: ${erro}`)
     })
 })
+
+app.get('/deletar/:id', function (req, res) {
+    Post.destroy({ where: { 'id': req.params.id } }).then(function (erro) {
+      res.send(`Post deletado com sucesso`)
+    }).catch(function (erro) {
+    res.send('Está mensagem não pode ser deletada')
+    })  
+  })
 
 app.listen(8081, function () {
   console.log('Servidor rodando na url http://localhost:8081')
